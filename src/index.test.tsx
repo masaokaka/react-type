be307c30714bd88a4c26dd063055970885775845
masaokaka <osushiiisensu@gmail.com>
2021-06-22 19:12:31 +0900

    変更

---
+++ src/index.test.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+
+test("renders learn react link", () => {
+  const { getByText } = render(
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
+  );
+
+  expect(getByText("該当する商品はありません")).toBeInTheDocument();
+});
